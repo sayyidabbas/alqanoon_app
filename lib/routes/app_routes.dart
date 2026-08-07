ab5e8c586c3486d8caae844b23215371544479ac
sayyidabbas <sayyidabbas.25lw0219@student.uomosul.edu.iq>
2026-08-07 15:36:49 +0300

    Create app_routes.dart

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../screens/splash_screen.dart';
+import '../screens/login_screen.dart';
+import '../screens/register_screen.dart';
+import '../screens/home_screen.dart';
+
+class AppRoutes {
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String home = '/home';
+
+  static Map<String, WidgetBuilder> get routes => {
+    splash: (context) => const SplashScreen(),
+    login: (context) => const LoginScreen(),
+    register: (context) => const RegisterScreen(),
+    home: (context) => const HomeScreen(),
+  };
+}
