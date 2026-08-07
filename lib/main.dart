f8fc6d1b611b0514fa732b42bd0a4b4d8c7b33ee
sayyidabbas <sayyidabbas.25lw0219@student.uomosul.edu.iq>
2026-08-07 14:34:36 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart';
 
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const LawPlatformApp());
+}
+
+class LawPlatformApp extends StatelessWidget {
+  const LawPlatformApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'منصة القانون',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: 'Cairo',
+        scaffoldBackgroundColor: const Color(0xff05070D),
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
