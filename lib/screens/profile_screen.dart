bc011abd72112f2b8236b6be097f616f0a8023b5
sayyidabbas <sayyidabbas.25lw0219@student.uomosul.edu.iq>
2026-08-09 02:46:07 +0300

    Update profile_screen.dart

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package0/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart'; // 🟢 تم تصحيح المسار بنجاح
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 import '../constants/app_colors.dart';
@@ -498,4 +498,3 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 }
- 
