d50de6a66115435b1235d56f82e54d29a422269a
sayyidabbas <sayyidabbas.25lw0219@student.uomosul.edu.iq>
2026-08-10 00:12:26 +0300

    Update app_routes.dart

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -3,17 +3,29 @@ import '../screens/splash_screen.dart';
 import '../screens/login_screen.dart';
 import '../screens/register_screen.dart';
 import '../screens/home_screen.dart';
+import '../services_screens/legal_library_screen.dart';
+import '../services_screens/study_materials_screen.dart';
+import '../services_screens/question_bank_screen.dart';
+import '../services_screens/electronic_exams_screen.dart';
 
 class AppRoutes {
   static const String splash = '/';
   static const String login = '/login';
   static const String register = '/register';
   static const String home = '/home';
+  static const String legalLibrary = '/legal_library';
+  static const String studyMaterials = '/study_materials';
+  static const String questionBank = '/question_bank';
+  static const String electronicExams = '/electronic_exams';
 
   static Map<String, WidgetBuilder> get routes => {
     splash: (context) => const SplashScreen(),
     login: (context) => const LoginScreen(),
     register: (context) => const RegisterScreen(),
     home: (context) => const HomeScreen(),
+    legalLibrary: (context) => const LegalLibraryScreen(),
+    studyMaterials: (context) => const StudyMaterialsScreen(),
+    questionBank: (context) => const QuestionBankScreen(),
+    electronicExams: (context) => const ElectronicExamsScreen(),
   };
 }
